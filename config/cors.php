605182c7e8386b8f75f984dcc175882801a01afe
Alejandro <alejandropino88@gmail.com>
2025-08-18 16:16:39 +0200

    Update CORS configuration to replace production allowed origin with new URL and refactor log-viewer.php for improved documentation and organization of settings

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000', 'https://shopping-list-alpha-lac.vercel.app', 'https://shopping-list-giantswings-projects.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000', 'https://basketi.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
